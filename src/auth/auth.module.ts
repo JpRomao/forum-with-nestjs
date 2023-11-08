4554cea001132bc26363be218713f375a4cd59c5
Joao Pedro Romao <ojpzinho@gmail.com>
2023-11-08 04:56:45 -0300

    create question controller created with jwt verification strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { JwtModule } from '@nestjs/jwt'
 import { ConfigService } from '@nestjs/config'
 
 import { Env } from 'src/env'
+import { JwtStrategy } from './jwt-strategy'
 
 @Module({
   imports: [
@@ -25,5 +26,6 @@ import { Env } from 'src/env'
       },
     }),
   ],
+  providers: [JwtStrategy],
 })
 export class AuthModule {}
